f15339e4a1e99e80d25c3c5f048e314e66c2d25b
Affluxx <Affluxxx@gmail.com>
2015-03-20 16:13:38 +0100

    la version de base du TP

--- src/tp/model/CityManager.java
+++ src/tp/model/CityManager.java
@@ -20,8 +20,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * </ul>
  *
  */
-@XmlRootElement
-@WebService(endpointInterface = "package.of.CityManagerService", serviceName = "CityManagerService")
+@WebService(endpointInterface = "tp.model.CityManagerService", serviceName = "CityManagerService")
 public class CityManager implements CityManagerService {
     // An attribute for the cities in the manager
 	private List<City> cities;
@@ -45,16 +44,6 @@ public class CityManager implements CityManagerService {
     // A method who define the list of cities
 
     /**
-     * Set the list of cities
-     * @param cities
-     * the new list of cities
-     */
-	public void setCities(List<City> cities) {
-		this.cities = cities;
-	}
-	// A method who add a city in the list
-
-    /**
      * Appends a new city at the end of the list
      * @param city
      * city to be appended to this list
@@ -76,77 +65,4 @@ public class CityManager implements CityManagerService {
 		return cities.remove(city);
 	}
 
-    /**
-     * Return a List of City with the name in parameter
-     * of the city. (As we know, a cityName can be use in many countries).
-     * Ex : Paris (in France) and Paris(in USA)
-     * @param cityName
-     * The name of the city
-     * @return tmp
-     * A list of cities.
-     */
-	public List<City> searchFor(String cityName){
-		// TODO DONE: à compléter
-		List<City> tmp = new ArrayList<City>();
-		for (City c : cities){
-			if(c.getName().equals(cityName)){
-				tmp.add(c);
-			}
-		}
-		return tmp;
-	}
-
-    /**
-     * Return the city at the exact position if it exist
-     * @param position
-     * The position of the city
-     * @return city
-     * The city if it exist
-     * @throws CityNotFound
-     * If the city don't exist we throw an exception
-     */
-	public City searchExactPosition(Position position) /* TODO throws CityNotFound */{
-        // We look all city
-		for(City city : getCities()){
-            // If one city have the exact position we return it
-			if (position.equals(city.getPosition())){
-				return city;
-			}
-		}
-        // If we don't find a result that mean we can not find a city at this position
-		//throw new CityNotFound();
-        return null;
-	}
-	
-	/**
-	 * TODO DONE: searchNear : une fonction qui retourne la liste des villes à dix klomètres d'une position
-	 */
-    /**
-     * With the function searcNear you can have a list of cities around a position
-     * @param p
-     * the parameter is the position
-     * @return tmp
-     * The return value is the list of cities around the parameter p.
-     */
-	public List<City> searchNear(Position p){
-		// At first we create a List to stock the result.
-		List<City> tmp = new ArrayList<City>();
-
-		//With the two variables we search all cities around the position
-		for (City c : getCities()){
-			//Here we have the test who validate the city
-            if(p.distanceTo(c.getPosition()) < 10000){
-				tmp.add(c);
-			}
-		}
-		// And after the research we return the result.
-		return tmp;
-	}
-
-    /**
-     * clear the city list
-     */
-    public void clear(){
-        cities.clear();
-    }
 }
--- src/tp/rest/MyServiceTP.java
+++ src/tp/rest/MyServiceTP.java
@@ -1,38 +1,22 @@
 package tp.rest;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.util.JAXBSource;
-import javax.xml.transform.Source;
 import javax.xml.ws.Endpoint;
-import javax.xml.ws.Provider;
 import javax.xml.ws.Service;
 import javax.xml.ws.ServiceMode;
 import javax.xml.ws.WebServiceContext;
-import javax.xml.ws.WebServiceException;
 import javax.xml.ws.WebServiceProvider;
-import javax.xml.ws.handler.MessageContext;
-import javax.xml.ws.http.HTTPBinding;
 
 import tp.model.City;
 import tp.model.CityManager;
-import tp.model.CityNotFound;
-import tp.model.Position;
 
-import java.text.NumberFormat;
-import java.text.ParseException;
 
 @WebServiceProvider
                    
 @ServiceMode(value=Service.Mode.MESSAGE)
-public class MyServiceTP implements Provider<Source> {
+public class MyServiceTP  {
 	
 	/**
 	 * Gère les villes
 	 */
-	private CityManager cityManager = new CityManager();
-	
-	private JAXBContext jc;
 	
 	@javax.annotation.Resource(type=Object.class)
 	protected WebServiceContext wsContext;
@@ -41,148 +25,9 @@ public class MyServiceTP implements Provider<Source> {
         cityManager.addCity(new City("Rouen",50,50,"FR"));
         Endpoint.publish("http://127.0.0.1:8084/citymanager", cityManager);
     }
-    /**
-	public MyServiceTP(){
-		try {
-            jc = JAXBContext.newInstance(CityManager.class,City.class,Position.class);
-            
-        } catch(JAXBException je) {
-            System.out.println("Exception " + je);
-            throw new WebServiceException("Cannot create JAXBContext", je);
-        }
-	}
-     */
-	 
-    public Source invoke(Source source) {
-    	
-        try{
-            MessageContext mc = wsContext.getMessageContext();
-            String path = (String)mc.get(MessageContext.PATH_INFO);
-            String method = (String)mc.get(MessageContext.HTTP_REQUEST_METHOD);
-            System.out.println("Got HTTP "+method+" request for "+path);
-		    if (method.equals("GET")) 
-	                return get(mc);
-			if (method.equals("POST")) 
-				    return post(source, mc);
-           	if (method.equals("PUT")) 
-					return put(source, mc); 
-           	if (method.equals("DELETE"))
-                try {
-                    return delete(source, mc);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-            throw new WebServiceException("Unsupported method:" +method);
-        } catch(JAXBException je) {
-            throw new WebServiceException(je);
-        }
-    }
-
-	private Source put(Source source, MessageContext mc) throws JAXBException {
-		// TODO DONE à compléter
-		// * ajouter une ville passée en paramètre au citymanager
-        Unmarshaller u = jc.createUnmarshaller();
-        City city =(City)u.unmarshal(source);
-
-        cityManager.addCity(city);
-
-        return new JAXBSource(jc, cityManager);
-	}
-
-	private Source delete(Source source, MessageContext mc) throws JAXBException, ParseException {
-
-		// TODO DONE à compléter
-		// * effacer la ville passée en paramètre
-
-        Unmarshaller u = jc.createUnmarshaller();
-        String path = (String) mc.get(MessageContext.PATH_INFO);
-        System.out.println("path : " + path);
-        if(path != null && path.equals("all")){
-            cityManager.clear();
-        } else {
-            String[] info = path.split("/");
-            City city;
-            city = new City(info[1],Double.parseDouble(info[3]),Double.parseDouble(info[2]), info[0]);
-            City found = null;
-            for(City c : cityManager.getCities()){
-                System.out.println("city c : " + c.toString());
-
-                if (c.getPosition().equals(city.getPosition())){
-                    found = c;
-                }
-            }
-            if(found == null){
-                System.out.println("city not found : " + city.toString());
-                //throw new CityNotFound();
-            } else {
-                System.out.println("city found");
-
-            }
-            cityManager.removeCity(found);
-        }
-        return new JAXBSource(jc, cityManager);
-	}
-
-	private Source post(Source source, MessageContext mc) throws JAXBException {
-		// * rechercher une ville à partir de sa position
-		Unmarshaller u = jc.createUnmarshaller();
-		Position position=(Position)u.unmarshal(source);
-        CityManager cities = new CityManager();
-        String path = (String) mc.get(MessageContext.PATH_INFO);
-        System.out.println("path : " + path);
-        if (path != null && path.equals("near")) {
-            for(City c :  cityManager.searchNear(position)){
-                cities.addCity(c);
-            }
-            return new JAXBSource(jc, cities);
-        } else {
-            cities.addCity(cityManager.searchExactPosition(position));
-            return new JAXBSource(jc, cities);
-        }
-		/*Object message;
-		try {
-		} catch (CityNotFound cnf) {
-			// TODO: retourner correctement l'exception
-			message = cnf;
-		}*/
-
-		// TODO DONE à compléter
-		// * rechercher les villes proches de cette position si l'url de post contient le mot clé "near"
-
-	}
-
-	private Source get(MessageContext mc) throws JAXBException {
-		// TODO DONE à compléter
-		// * retourner seulement la ville dont le nom est contenu dans l'url d'appel
-		// * retourner tous les villes seulement si le chemin d'accès est "all"
-
-        String path = (String) mc.get(MessageContext.PATH_INFO);
-        CityManager cities = new CityManager();
-        System.out.println(path);
-        if (path == null || path.equals("all")) {
-            System.out.println("ALL");
-            return new JAXBSource(jc, cityManager);
-        } else {
-            String[] info = path.split("/");
-            for (String name : info){
-                System.out.println("name : " + name);
-                for (City c : cityManager.getCities()){
-                    if(c.getName().equals(name)){
-                        cities.addCity(c);
-                    }
-                }
-            }
-        }
-        return new JAXBSource(jc, cities);
-    }
 
 	public static void main(String args[]) throws InterruptedException {
-        /*
-	      Endpoint e = Endpoint.create(HTTPBinding.HTTP_BINDING,
-	                                     new MyServiceTP());
-	      e.publish("http://127.0.0.1:8084/");
-	       // pour arrêter : e.stop();
-    */
+
         new MyServiceTP();
         Thread.sleep(15 * 60 * 1000);
         System.exit(0);
